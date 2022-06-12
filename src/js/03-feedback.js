e1b577a346d51178255300ba280de93cbb6feb30
Maksim-Yaroschuk <79583144+Maksim-Yaroschuk@users.noreply.github.com>
2022-06-12 21:19:24 +0300

    optimize task#3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,67 +1,62 @@
 import throttle from 'lodash.throttle';
 
 const feedbackForm = document.querySelector(".feedback-form")
+const {email, message} = feedbackForm
 
 const STORAGE_KEY = "feedback-form-state";
 
 restoreForm()
 
 feedbackForm.addEventListener("input", throttle(onInputForm, 500))
+feedbackForm.addEventListener("submit", onSubmitForm )
 
-function onInputForm(event) {
-     const {
-        elements: { email, message }
-    } = event.currentTarget 
+function onInputForm() {
+  
+    // Повторення коду, не можу додуматись як його позбутись
+    const feedback = {
+        email: email.value,
+        message: message.value
+    }
+    
+    // collectedObject()
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(feedback));
+}
 
+function onSubmitForm(event) {
+  event.preventDefault()
+
+      if (email.value === "" || message.value === "") {
+    return alert("Всі поля повинні бути заповнені!");
+      }
+  
+    // Повторення коду, не можу додуматись як його позбутись
   const feedback = {
     email: email.value,
     message: message.value
   }
 
-    console.log(feedback)
-localStorage.setItem(STORAGE_KEY, JSON.stringify(feedback));
+    // collectedObject()
+  console.log(feedback)
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
 }
-
 
 function restoreForm() {
   const savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
     if (savedForm) {
         console.log("запонюється форма")
-    feedbackForm.email.value = savedForm.email
-    feedbackForm.message.value = savedForm.message
+
+        email.value = savedForm.email
+        message.value = savedForm.message
     }
-
-
-    
-//     console.log(savedForm.email)
-//     console.log(savedForm.message)
-// console.log(feedbackForm.email.value)
 }
 
-
-
-feedbackForm.addEventListener("submit", onCollectFeedback )
-
-function onCollectFeedback(event) {
-  event.preventDefault()
-  
-  const {
-        elements: { email, message }
-    } = event.currentTarget
-
-      if (email.value === "" || message.value === "") {
-    return alert("Всі поля повинні бути заповнені!");
-      }
-  
-  const feedback = {
-    email: email.value,
-    message: message.value
-  }
-
-  console.log(feedback)
-    event.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-}
-
+// function collectedObject() {
+//       const feedback = {
+//             email: email.value,
+//             message: message.value
+//       }
+//     return feedback
+// }
 
